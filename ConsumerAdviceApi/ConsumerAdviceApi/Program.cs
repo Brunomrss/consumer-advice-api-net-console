36b5705921ebe7a32ac881fe4b640baf65479eec
Bruno <142849905+Brunomrss@users.noreply.github.com>
2025-10-01 21:02:47 -0300

    Add files via upload

--- ConsumerAdviceApi/ConsumerAdviceApi/Program.cs
+++ ConsumerAdviceApi/ConsumerAdviceApi/Program.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Net.Http;
-using System.Threading.Tasks;
-using System.Text.Json;
+﻿using System.Text.Json;
+using ConsumerAdviceApi.Models;
 
-namespace ConsumerAdviceApi
-{
-    class Program
-    {
-        static async Task Main(string[] args)
-        {
-            Console.WriteLine("Conectando à API de conselhos...\n");
+Console.WriteLine("Conectando à API de conselhos...\n");
 
             try
             {
@@ -17,30 +9,16 @@ namespace ConsumerAdviceApi
                 string url = "https://api.adviceslip.com/advice";
 
                 HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode(); 
+                response.EnsureSuccessStatusCode();
 
                 string responseBody = await response.Content.ReadAsStringAsync();
 
                 var adviceObject = JsonSerializer.Deserialize<AdviceResponse>(responseBody);
 
                 Console.WriteLine("Conselho de Hoje:");
-                Console.WriteLine(adviceObject.slip.advice);
+                Console.WriteLine(adviceObject?.slip?.advice ?? "Nenhum conselho encontrado.");
             }
             catch (Exception ex)
             {
                 Console.WriteLine($"Erro ao acessar a API: {ex.Message}");
             }
-        }
-    }
-
-    public class AdviceResponse
-    {
-        public Slip slip { get; set; }
-    }
-
-    public class Slip
-    {
-        public int id { get; set; }
-        public string advice { get; set; }
-    }
-}
